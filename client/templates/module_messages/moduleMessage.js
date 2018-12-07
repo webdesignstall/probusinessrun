d44e9db727991faf7d73e47e70cb35a7d655aebe
Yusif <joseph.khalilov@gmail.com>
2018-12-07 13:56:23 -0800

    Added push.js browser notification

--- client/templates/module_messages/moduleMessage.js
+++ client/templates/module_messages/moduleMessage.js
@@ -1,21 +1,34 @@
 import { Tracker } from 'meteor/tracker';
 import Discounts from '../../../common/discountData';
 import { Template } from 'meteor/templating';
+import Push from 'push.js';
 
 Template.modalMesaj.onRendered(() => {
     Tracker.autorun(() => {
         Meteor.subscribe('Dicsounts');
         const discounts = Discounts.find({}).fetch();
+        console.log("​discounts", discounts)
 
         discounts.length > 0
-            ? Bert.alert({
-                title: 'Discount asking',
-                message: `Truck #${ discounts[discounts.length - 1].truckNumber } asking for discount`,
-                type: 'danger',
-            })
+            ? (
+                Bert.alert({
+                    title: 'Discount asking',
+                    message: `Truck #${ discounts[discounts.length - 1].truckNumber } asking for discount`,
+                    type: 'danger',
+                }),
+                Push.create(`Truck #${ discounts[discounts.length - 1].truckNumber } asking for discount`, {
+                    timeout: 120000,
+                    onClick: function () {
+                        link: 'http://localhost:3000/quote'
+                        window.focus();
+                        this.close();
+                    }
+                })
+            )
             : null;
+
     })
-})
+});
 
 function modalMessage(mesaj) {
     document.getElementById('mesaj').innerHTML = '<div class="modal-messages"><div class="not-header">Notification!</div><div class="not-mesaj">' + mesaj + '</div></div>';
