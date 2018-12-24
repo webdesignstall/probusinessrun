eaa4cc3ac4883a9ba099de8e84d23a05b8117080
Yusif <joseph.khalilov@gmail.com>
2018-12-24 11:59:05 -0800

    This fixes #46 #47 confirmation uncorrect displaying

--- client/templates/reserve_quote/ConfirmationDisplay.js
+++ client/templates/reserve_quote/ConfirmationDisplay.js
@@ -1,87 +1,95 @@
 import React, { Component } from 'react';
+import { Session } from 'meteor/session';
 
 export default class ConfirmationDisplay extends Component {
+    constructor(props) {
+        super(props);
+
+        // FIXME: sessionda oalnda ancaq state elave olsun 
+        this.state = {
+            company: Object.keys(Session.get('job')).length > 0 ? Session.get('job').company : {}
+        }
+    }
     render() {
         return (
-            <div>
-                <div style="text-align: center;">
-                    <span style="color: #ffffff; font-size: xx-large;">
+            <div className="center-text">
+                <div>
+                    <span>
                         <strong>Thank you for booking your move with us!&nbsp;</strong>
                     </span>
                 </div>
                 <div>
-                    <span style="color: #ffffff; font-size: xx-large;">
+                    <span>
                         <strong>&nbsp;</strong>
                     </span>
                 </div>
                 <div>
-                    <span style="color: #ffffff; font-size: xx-large;">
+                    <span>
                         <strong>&nbsp;</strong>
                     </span>
                 </div>
-                <div style="text-align: center;">
-                    <span style="color: #ffffff; font-size: large;">
+                <div>
+                    <span>
                         <strong>&nbsp;</strong>
                     </span>
                 </div>
-                <div style="text-align: center;">
-                    <span style="color: #ffffff; font-size: large;">
+                <div>
+                    <span>
                         <strong>Confirmation e-mail was sent to your e-mail address.&nbsp;</strong>
                     </span>
                 </div>
-                <div style="text-align: center;">
-                    <span style="color: #ffffff; font-size: large;">
+                <div>
+                    <span>
                         <strong>&nbsp;</strong>
                     </span>
                 </div>
-                <div style="text-align: center;">
-                    <span style="color: #ffffff; font-size: large;">
+                <div>
+                    <span>
                         <strong>You can always call us, e-mail and text if you had any questions.&nbsp;</strong>
                     </span>
                 </div>
-                <div style="text-align: center;">
-                    <span style="color: #ffffff; font-size: large;">
+                <div>
+                    <span>
                         <strong>&nbsp;</strong>
                     </span>
                 </div>
-                <div style="text-align: center;">
-                    <span style="color: #ffffff; font-size: large;">
+                <div>
+                    <span>
                         <strong>Hoping to see you soon!&nbsp;</strong>
                     </span>
                 </div>
-                <div style="text-align: center;">
-                    <span style="color: #ffffff; font-size: large;">
+                <div>
+                    <span>
                         <strong>Your Trusted Movers&nbsp;</strong>
                     </span>
                 </div>
-                <div style="text-align: center;">
+                <div>
                     <div>&nbsp;</div>
                 </div>
-                <div style="text-align: center;">&nbsp;</div>
-                <div style="text-align: center;">
+                <div>&nbsp;</div>
+                <div>
                     <div>&nbsp;</div>
                 </div>
-                <div style="text-align: center;">
+                <div>
                     <div>
-                        <span style="color: #ffffff;">Cheap Movers Los Angeles -- Moving you with pride since 2006!</span>
+                        <span>{this.state.company.name} -- Moving you with pride since 2006!</span>
                     </div>
                 </div>
                 <div>
-                    <div style="text-align: center;">
-                        <span style="color: #ffffff;">Phone:&nbsp;
-                    <a style="color: #ffffff;" href="tel:(213)%20262-9440" target="_blank" rel="noopener">213 262 9440</a>&nbsp;|
-                                    E-mail:&nbsp;
-                    <a style="color: #ffffff;" href="mailto:info@cheapmoverslosangeles.com" target="_blank" rel="noopener">info@cheapmoverslosangeles.com</a>
+                    <div>
+                        <span>Phone:&nbsp;
+                    <a href={'tel:' + this.state.company.phone} target="_blank" rel="noopener">this.state.company.phone</a>&nbsp;<br />
+                            E-mail:&nbsp;
+                    <a href={'mailto:' + this.state.company.email} target="_blank" rel="noopener">{this.state.company.email}</a>
                         </span>
                     </div>
-                    <div style="text-align: center;">
-                        <span style="color: #ffffff;">Web:&nbsp;
-                    <a style="color: #ffffff;" href="http://www.cheapmoverslosangeles.com/" target="_blank" rel="noopener"
-                                data-saferedirecturl="https://www.google.com/url?hl=en&amp;q=http://www.CheapMoversLosAngeles.com&amp;source=gmail&amp;ust=1521936616182000&amp;usg=AFQjCNE5VesaUK3eo7urqxRTI6CdZQ2jNA">http://www.<wbr />CheapMoversLosAngeles.com</a>
+                    <div>
+                        <span>Web:&nbsp;
+                    <a href={this.state.company.website} target="_blank" rel="noopener">{this.state.company.website}</a>
                         </span>
                     </div>
-                    <div style="text-align: center;">
-                        <span style="color: #ffffff;">CAL PUC T-0191555</span>
+                    <div>
+                        <span>CAL PUC T-0191555</span>
                     </div>
                 </div>
             </div>
