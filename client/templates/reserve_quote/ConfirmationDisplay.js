8345411880fd88c77de6f762020e39e21bb15630
YusifKhalilov <joseph.khalilov@gmail.com>
2019-05-07 19:12:24 -0700

    fixed typo had -> have

--- client/templates/reserve_quote/ConfirmationDisplay.js
+++ client/templates/reserve_quote/ConfirmationDisplay.js
@@ -17,7 +17,9 @@ export default class ConfirmationDisplay extends Component {
             <div className="center-text">
                 <div>
                     <span>
-                        <strong>Thank you for booking your move with us!&nbsp;</strong>
+                        <strong>
+                            Thank you for booking your move with us!&nbsp;
+                        </strong>
                     </span>
                 </div>
                 <div>
@@ -37,7 +39,10 @@ export default class ConfirmationDisplay extends Component {
                 </div>
                 <div>
                     <span>
-                        <strong>Confirmation e-mail was sent to your e-mail address.&nbsp;</strong>
+                        <strong>
+                            Confirmation e-mail was sent to your e-mail
+                            address.&nbsp;
+                        </strong>
                     </span>
                 </div>
                 <div>
@@ -47,7 +52,10 @@ export default class ConfirmationDisplay extends Component {
                 </div>
                 <div>
                     <span>
-                        <strong>You can always call us, e-mail and text if you had any questions.&nbsp;</strong>
+                        <strong>
+                            You can always call us, e-mail and text if you have
+                            any questions.&nbsp;
+                        </strong>
                     </span>
                 </div>
                 <div>
@@ -74,20 +82,48 @@ export default class ConfirmationDisplay extends Component {
                 </div>
                 <div>
                     <div>
-                        <span>{this.state.company.name} -- Moving you with pride since 2006!</span>
+                        <span>
+                            {this.state.company.name} -- Moving you with pride
+                            since 2006!
+                        </span>
                     </div>
                 </div>
                 <div>
                     <div>
-                        <span>Phone:&nbsp;
-                            <a style={colorStyle} href={'tel:' + this.state.company.phoneNumber} target="_blank" rel="noopener noreferrer">{this.state.company.phoneNumber}</a>&nbsp;<br />
+                        <span>
+                            Phone:&nbsp;
+                            <a
+                                style={colorStyle}
+                                href={'tel:' + this.state.company.phoneNumber}
+                                target="_blank"
+                                rel="noopener noreferrer">
+                                {this.state.company.phoneNumber}
+                            </a>
+                            &nbsp;
+                            <br />
                             E-mail:&nbsp;
-                            <a style={colorStyle} href={'mailto:' + this.state.company.email} target="_blank" rel="noopener noreferrer">{this.state.company.email}</a>
+                            <a
+                                style={colorStyle}
+                                href={'mailto:' + this.state.company.email}
+                                target="_blank"
+                                rel="noopener noreferrer">
+                                {this.state.company.email}
+                            </a>
                         </span>
                     </div>
                     <div>
-                        <span>Web:&nbsp;
-                            <a style={{ textDecoration: 'none', color: 'white' }} href={'http://' + this.state.company.url} target="_blank" rel="noopener noreferrer">{'http://' + this.state.company.url}</a>
+                        <span>
+                            Web:&nbsp;
+                            <a
+                                style={{
+                                    textDecoration: 'none',
+                                    color: 'white'
+                                }}
+                                href={'http://' + this.state.company.url}
+                                target="_blank"
+                                rel="noopener noreferrer">
+                                {'http://' + this.state.company.url}
+                            </a>
                         </span>
                     </div>
                     <div>Powered by Movers Legion</div>
