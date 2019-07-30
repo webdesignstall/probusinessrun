1fd5253361bfd062ef94ab767fb1b6c5fa233d34
Yusif Khalilov <yusifkhalilov@yusifs-mbp.home>
2019-07-29 17:46:03 -0700

    Fixed typo

--- client/templates/cardHolder/CardHolderMessage.js
+++ client/templates/cardHolder/CardHolderMessage.js
@@ -13,7 +13,7 @@ class CardHolderMessage extends Component {
                 fnomlt: (flatRate, jobNumber) =>
                     `Amount to Charge: $${flatRate} flat charge plus if any additional charge for listed moving services with the job number: ${jobNumber}`,
                 hmlt: (rate, laborTime, jobNumber) =>
-                    `Amount to Charge:  $${rate} per hour with ${laborTime} hours minimum charge plus if any a. dditional charge for listed moving services with the job number: ${jobNumber}`,
+                    `Amount to Charge:  $${rate} per hour with ${laborTime} hours minimum charge plus if any additional charge for listed moving services with the job number: ${jobNumber}`,
                 hnomlt: (rate, jobNumber) =>
                     `Amount to Charge:  $${rate} per hour charge plus if any additional charge for listed moving services with the job number: ${jobNumber}`
             }
@@ -30,12 +30,7 @@ class CardHolderMessage extends Component {
 
     renderMessage(job) {
         let laborTime = job.laborTime || null;
-        let flatRate =
-            (job.flatRate &&
-                job.flatRate[0] &&
-                job.flatRate[0].isTrue &&
-                job.flatRate[0].cardAmount) ||
-            null;
+        let flatRate = (job.flatRate && job.flatRate[0] && job.flatRate[0].isTrue && job.flatRate[0].cardAmount) || null;
         let jobNumber = job.jobNumber;
         let rate = job.hourlyRatesCard || null;
 
@@ -50,13 +45,11 @@ class CardHolderMessage extends Component {
             <div className="col s12 m6 l6 offset-m3 offset-l3">
                 <p>{this.state.job && this.renderMessage(this.state.job)}</p>
                 <p>
-                    I authorize {this.state.job && this.state.job.companyInfo.name} to charge the
-                    amount listed above to the credit card provided herein on my behalf. I agree to
-                    pay for this purchase in accordance with the issuing bank cardholder agreement.
-                    I understand this move is scheduled for{' '}
-                    {this.state.job && this.state.job.clientFirstName}{' '}
-                    {this.state.job && this.state.job.clientLastName} and I agree to pay behalf of{' '}
-                    {this.state.job && this.state.job.clientFirstName}{' '}
+                    I authorize {this.state.job && this.state.job.companyInfo.name} to charge the amount listed above to the
+                    credit card provided herein on my behalf. I agree to pay for this purchase in accordance with the issuing bank
+                    cardholder agreement. I understand this move is scheduled for{' '}
+                    {this.state.job && this.state.job.clientFirstName} {this.state.job && this.state.job.clientLastName} and I
+                    agree to pay behalf of {this.state.job && this.state.job.clientFirstName}{' '}
                     {this.state.job && this.state.job.clientLastName}.
                 </p>
             </div>
