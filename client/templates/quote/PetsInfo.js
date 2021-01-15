87eaba16e98726572e85c6d493a39dc2f1cbc757
YusifKhalilov <joseph.khalilov@gmail.com>
2021-01-15 13:28:42 -0800

    Fixed bug when clikcing pets button disabling all others

--- client/templates/quote/PetsInfo.js
+++ client/templates/quote/PetsInfo.js
@@ -17,6 +17,11 @@ function PetsInfo() {
         job.pets ? (job.pets = false) : (job.pets = true);
 
         Session.set('job_', job);
+
+        let btns = document.getElementsByClassName('btn');
+        for (let i = 0; i < btns.length; i++) {
+            btns[i].classList.remove('disabled');
+        }
     }, [pets]);
 
     return (
