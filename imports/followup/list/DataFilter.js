1d4ddbdd115a59f99c2400f1f4d57b8df1b5066c
YusifKhalilov <joseph.khalilov@gmail.com>
2021-05-26 19:17:49 -0700

    This fixes #378 fixed sort by functionality

--- imports/followup/list/DataFilter.js
+++ imports/followup/list/DataFilter.js
@@ -74,6 +74,7 @@ function DataFilter() {
 		}
 	}, [searchWordsArray]);
 
+	/* ------------------------------ WORDS CLEANUP ----------------------------- */
 	// clean up words from extra characters and returns array of words
 	const wordsCleanUp = () => {
 		let arrayOfWords = searchWord.split(' ');
@@ -108,35 +109,36 @@ function DataFilter() {
 		});
 	};
 
-	// sorting data
+	/* ------------------------------ DATA SORTING ------------------------------ */
+
 	const sortingData = () => {
 		let jobListData = [...jobList];
 		jobListData.sort((a, b) => {
 			if (sorting === 'default') {
 				return (
-					new Date(b.statusChange || '1 november 1989').getTime() -
-					new Date(a.statusChange || '1 november 1989').getTime()
+					moment(b.statusChange || '1 november 1989') -
+					moment(a.statusChange || '1 november 1989')
 				);
 			}
 
 			if (sorting === 'az') {
 				return (
-					new Date(a.workDate || '1 november 1989').getTime() -
-					new Date(b.workDate || '1 november 1989').getTime()
+					moment(a.workDate || '1 november 1989') -
+					moment(b.workDate || '1 november 1989')
 				);
 			}
 
 			if (sorting === 'za') {
 				return (
-					new Date(b.workDate || '1 november 1989').getTime() -
-					new Date(a.workDate || '1 november 1989').getTime()
+					moment(b.workDate || '1 november 1989') -
+					moment(a.workDate || '1 november 1989')
 				);
 			}
 
 			if (sorting === 'lc') {
 				return (
-					new Date(b.lastChange || '1 november 1989').getTime() -
-					new Date(a.lastChange || '1 november 1989').getTime()
+					moment(b.lastChange || '1 november 1989') -
+					moment(a.lastChange || '1 november 1989')
 				);
 			}
 		});
@@ -148,81 +150,6 @@ function DataFilter() {
 	useEffect(() => {
 		sortingData();
 	}, [sorting]);
-
-	// const subscSearchWords = () => {
-	// 	dataSub && dataSub.stop(); // stop subscription for gettin new one
-	// 	Meteor.subscribe('searchFollowUp', arrayOfWords);
-	// };
-
-	// const fetchConvertedWord = reg => {
-	// 	return WorkData.find(
-	// 		{
-	// 			$or: [
-	// 				{
-	// 					clientFirstName: {
-	// 						$in: reg
-	// 					}
-	// 				},
-	// 				{
-	// 					clientLastName: {
-	// 						$in: reg
-	// 					}
-	// 				},
-	// 				{
-	// 					jobNumber: {
-	// 						$in: reg
-	// 					}
-	// 				},
-	// 				{
-	// 					phoneNumber: {
-	// 						$in: reg
-	// 					}
-	// 				}
-	// 			]
-	// 		},
-	// 		{ limit: showLimit, sort: { _id: -1 } }
-	// 	).fetch();
-	// };
-
-	// // on change of search word set new data
-	// useEffect(() => {
-	// 	if (shouldRenderSet) {
-	// 		if (!(searchWord.length > 0)) {
-	// 			setLoading(false);
-	// 			subscriber(fetchJobs);
-	// 		} else {
-	// 			// subscribe to new data
-	// 			subscSearchWords();
-
-	// 			let reg = wordsCleanUp(); // clean up words from problematic characters
-	// 			let resultConverted = fetchConvertedWord(reg); // find data matchin specific fields
-
-	// 			if (Array.isArray(resultConverted) && resultConverted.length > 0) {
-	// 				setJobList(resultConverted);
-	// 				setLoading(false);
-	// 			} else {
-	// 				console.log(
-	// 					`🚀 ~ file: DataFilter.js ~ line 98 ~ useEffect ~ resultConverted`,
-	// 					resultConverted
-	// 				);
-	// 				subscriber(fetchJobs);
-	// 				setLoading(false);
-	// 			}
-
-	// 			// arrayOfWords.length > 0
-	// 			// 	? Session.set('isSearch', true)
-	// 			// 	: Session.set('isSearch', false);
-	// 			shouldRenderSet = false; // prevent from re calculating
-	// 		}
-	// 	}
-	// }, [searchWord]);
-
-	// // subscribe on param change
-	// useEffect(() => {
-	// 	subscriber(); // subscribe to specific data
-	// }, [param]);
-
-	// component did mount
 
 	return null;
 }
